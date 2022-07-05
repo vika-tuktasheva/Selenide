851492d18f0dd73d0d04347bec880cb3b5ee6f43
vika-tuktasheva <konovalova_v@list.ru>
2022-07-05 22:38:31 +0400

    Init commit

---
+++ src/test/java/CardOrderTest.java
@@ -0,0 +1,86 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardOrderTest {
+
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:9999/");
+    }
+
+    @Test //тест с валидными значениями
+    public void shouldSendForm() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Санкт-Петербург");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+78884545321");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        //проверка видимости уведомления
+        $("[data-test-id=notification]").should(visible, Duration.ofSeconds(15));
+        $("[class=notification__content]").should(text("Встреча успешно забронирована на "+$("[data-test-id=date] input").getValue()));
+    }
+
+    @Test //ввод неверного города
+    public void shouldSendFormWhenWrongCity() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Новокузнецк");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+78884545321");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $$("[class=input__sub]").get(0).should(Condition.text("Доставка в выбранный город недоступна"), Duration.ofSeconds(15));
+    }
+
+    @Test //ввод неверной даты
+    public void shouldSendFormWhenWrongDate() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Санкт-Петербург");
+        $("[data-test-id=date] input").setValue("01.05.2022");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+78884545321");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $$("[class=input__sub]").get(1).should(text("Заказ на выбранную дату невозможен"), Duration.ofSeconds(15));
+    }
+
+    @Test //ввод неверного ФИО
+    public void shouldSendFormWhenWrongName() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Санкт-Петербург");
+        $("[data-test-id=name] input").setValue("Иванов Иван15");
+        $("[data-test-id=phone] input").setValue("+78884545321");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $$("[class=input__sub]").get(2).should(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."), Duration.ofSeconds(15));
+    }
+
+    @Test //ввод неверного телефона
+    public void shouldSendFormWhenWrongPhone() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Санкт-Петербург");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+7888454532156");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $$("[class=input__sub]").get(3).should(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."), Duration.ofSeconds(15));
+    }
+
+    @Test //нет согласия
+    public void shouldSendFormТoСonsent() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Санкт-Петербург");
+//        $("[data-test-id=date] input").setValue("08.07.2022");
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+78884545321");
+//        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[class=checkbox__text]").should(text("Я соглашаюсь с условиями обработки и использования моих персональных данных"), Duration.ofSeconds(15));
+    }
+}
